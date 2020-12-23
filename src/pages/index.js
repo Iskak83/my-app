e6253969c04d084480ef188ea28fea9ff7fbff24
Iskak Mantyubetov <ayzik55@gmail.com>
2020-12-22 23:56:44 -0500

    added some images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
 import * as React from "react"
 import Layout from "../components/layout"
-import "./education.scss"
+import "./index.scss"
+import img1 from "../images/ala-archa.jpg"
+import img2 from "../images/cat.jpg"
+import img3 from "../images/nitto-final.jpg"
+import img4 from "../images/vertual-grad.jpg"
 
 // styles
 
@@ -16,6 +20,8 @@ const Home = () => {
                 This is Home Page!
                 <br />
             </h1>
+            <div className="my-images"/>
+                 
         </div>
     </Layout>
   )
