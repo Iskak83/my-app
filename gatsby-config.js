220431abbf61c48a57137cac7129a4b9542dd70a
Iskak Mantyubetov <ayzik55@gmail.com>
2021-01-01 02:43:11 -0500

    added some styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
 				background_color: `#663399`,
 				theme_color: `#663399`,
 				display: `minimal-ui`,
-				icon: `src/images/my-logo.jpg`
+				icon: `src/images/logo.png`
 			}
     },
     "gatsby-plugin-smoothscroll"
