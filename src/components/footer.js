725c02c8055cf285b7cc6fc64855739372770b97
Iskak Mantyubetov <ayzik55@gmail.com>
2021-01-04 15:42:02 -0500

    changed image url address

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub, faTwitter, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import './footer.scss';
 
 const Footer = () => {
@@ -10,7 +10,7 @@ const Footer = () => {
 	return (
 		<footer className="footer-links" id="footer">
 
-			{/* <div > */}
+		
 				<a href="mailto: ayzik55@gmail.com">
 					<FontAwesomeIcon icon={faEnvelope} />
 				</a>
@@ -23,7 +23,13 @@ const Footer = () => {
 				<a href="https://twitter.com/iskak" target="_blank" rel="noreferrer">
 					<FontAwesomeIcon icon={faTwitter} />
 				</a>
-			{/* </div> */}
+				<a href="https://www.instagram.com/iskakmu/" target="_blank" rel="noreferrer">
+					<FontAwesomeIcon icon={faInstagram} />
+				</a>
+				<a href="https://www.facebook.com/iskak.mantyubetov" target="_blank" rel="noreferrer">
+					<FontAwesomeIcon icon={faFacebook} />
+				</a>
+			
 		</footer>
 	);
 };
