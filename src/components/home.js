9a72e2b5a59f3e350956d54baca61e276ff4a73d
Iskak Mantyubetov <ayzik55@gmail.com>
2021-01-04 13:46:23 -0500

    changed image url address

--- src/components/home.js
+++ src/components/home.js
@@ -9,7 +9,7 @@ const Home = () => {
         	<h2>Iskak Mantyubetov</h2>
             <Typewriter
                 options={{
-                    strings: ['Software Engineer', 'Hiker', 'Cook', 'Pet lover', 'Tennis fan'],
+                    strings: ['Software Engineer', 'Hiker', 'Cook', 'Pet lover', 'Traveller', 'Tennis fan'],
                     autoStart: true,
                     loop: true
                 }}
