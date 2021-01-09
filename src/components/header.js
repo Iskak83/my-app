cda27d5ab772cf385d9508491389e1eb10833c4c
Iskak Mantyubetov <ayzik55@gmail.com>
2021-01-08 19:36:05 -0500

    changed some styling on bio page

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 import "./header.scss"
 import scrollTo from 'gatsby-plugin-smoothscroll';
 
 export default function Header() {
+  const [burger, setBurger] = useState(true)
+  function onClick(idName){
+    if(!burger)setBurger(true)
+    if(idName !== '') scrollTo(idName)
+  }
+
   return (
   <div id="nav-bar">
     <div id="nav-left">
       <div id="nav-logo">
-        <div id="burger">
-          <div id="lineOne"></div>
-          <div id="lineTwo"></div>
-          <div id="lineThree"></div>
+        <div id="burger-container">
+          <button onClick={() => setBurger(!burger)} id={burger ? "burger" : "x"}>
+          
+            <div id="lineOne"></div>
+            <div id="lineTwo"></div>
+            <div id="lineThree"></div>
+          </button>
         </div>
         
-        <button onClick={() => scrollTo('#home')} id="logo" >im</button>
+        <button onClick={() => onClick('#home')} id="logo" >im</button>
       </div>
       <div id="divider"/>
     </div>
-    <div id="nav-links"> 
+    <div id={!burger ? "nav-links" : "nav-links-mobile"}> 
      
-							<button onClick={() => scrollTo('#bio')}>Bio</button>
-              <button onClick={() => scrollTo('#projects')}>Projects</button>
-              <button onClick={() => scrollTo('#education')}>Education</button>
-              <button onClick={() => scrollTo('#hobby')}>Hobbies</button>
+							<button onClick={() => onClick('#bio')}>Bio</button>
+              <button onClick={() => onClick('#projects')}>Projects</button>
+              <button onClick={() => onClick('#education')}>Education</button>
+              <button onClick={() => onClick('#hobby')}>Hobbies</button>
 				
 					
     </div>
