0d3c306464fe81c93400e12003f22fd05210f805
Iskak Mantyubetov <ayzik55@gmail.com>
2021-01-12 19:34:21 -0500

    added typwriting modulo

---
+++ src/components/TypeWriter.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from 'react'
+
+const TypeWriter = ({texts, speed, endSpeed, className}) => {
+    const [text, setText] = useState('')
+
+  useEffect(() => {
+      
+        let i = 0;
+        let isDeleting  = false;
+        let txt = '';
+
+        const  rotateText = () => {
+          
+           let string = texts[i]
+           let newSpeed = speed
+            if(isDeleting){
+                txt = string.slice(0, txt.length - 1)
+                setText(txt)   
+         
+            }else{
+                txt = string.slice(0, txt.length + 1)
+                setText(txt) 
+                console.log('....',i, txt)
+            }
+        //    
+            if(!isDeleting && txt === texts[i]){
+                newSpeed = endSpeed
+                isDeleting = true
+                console.log('>>>>>>',i, txt)
+            }else if(isDeleting && txt === ''){
+                console.log('<<<<<<',i, txt)
+                isDeleting = false
+                newSpeed = speed*3
+                i++
+                if(i === texts.length) i = 0
+            }else if(isDeleting && txt === texts[i]){
+                newSpeed = speed / 2
+            }
+
+            setTimeout(() => rotateText(), newSpeed)
+        }
+     rotateText()
+
+    }, [texts, speed, endSpeed])
+   
+
+    return(
+        <div className={className}>{text}</div>
+    )
+}
+export default TypeWriter
--- src/components/home.js
+++ src/components/home.js
@@ -1,22 +1,29 @@
 import * as React from "react"
-import Typewriter from 'typewriter-effect';
+// import Typewriter from 'typewriter-effect';
+import TypeWriter from './TypeWriter'
 import "./home.scss"
 
 
 const Home = () => {
+
+  const texts = ['Trilinguist', 'Tennis fan','Cook', 'Pet lover', 'Hiker', 'Traveller']
+  const speed = 150
+  const endSpeed = 2000
+  const className = 'type-writer'
   return (
     <div id="home">
       <div className="home">
         	<h1>Iskak Mantyubetov</h1>
           <h3>Full stack software engineer</h3>
           <h6>[‘JavaScript’, ‘React’, ‘Redux’, ‘Express’, ‘Sequelize’, ‘HTML’, ‘CSS’]</h6>
-            <Typewriter
+          <TypeWriter texts={texts} speed={speed} endSpeed={endSpeed} className={className} />
+            {/* <Typewriter
                 options={{
                     strings: ['Trilinguist', 'Tennis fan', 'Cook', 'Pet lover', 'Hiker', 'Traveller'],
                     autoStart: true,
                     loop: true
-                }}
-            />
+                }} */}
+            {/* /> */}
 				</div>	
     </div>
   )
