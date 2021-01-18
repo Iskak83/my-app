f67f612fb9609a162f316a3d75fef101e3553011
Iskak Mantyubetov <ayzik55@gmail.com>
2021-01-17 23:40:05 -0500

    added new language

--- src/components/bio.js
+++ src/components/bio.js
@@ -3,11 +3,12 @@ import scrollTo from 'gatsby-plugin-smoothscroll';
 import {myStory} from './data'
 import {myStoryRU} from './data-ru'
 import RotateImages from './RotateImages'
+import {LanguageContext}  from '../pages/index'
 import "./bio.scss"
 
-
-
 const Bio = () => {
+
+  const {state, change} = React.useContext(LanguageContext)
 
   const [readBttn, setReadBttn] = React.useState(true);
   
@@ -19,19 +20,18 @@ const Bio = () => {
   const images = [`ala-archa.png`, `parade.png`,
   `buckingham.png`, `virtual-grad.png`]
   
-  const imgInfo = ['Ala-Archa Mountain, Kyrgyzstan', `Parade 2019, NYC`,
-  `Buckingham Palace, London`, `Fullstack virtual graduation, South Orange`]
-  
-
-  const imgInfoRU = ['Горы Ала-Арча, Кыргызстан.', `Парад 2019, NYC`,
+  const imgInfo =  state.language === "english" ?['Ala-Archa Mountain, Kyrgyzstan', `Parade 2019, NYC`,
+  `Buckingham Palace, London`, `Fullstack virtual graduation, South Orange`] : ['Горы Ала-Арча, Кыргызстан.', `Парад 2019, NYC`,
   `Buckingham Palace, London`, `Виртуальный выпускной из Fullstack, Fullstack South Orange`]
+
+  const bioInfo = state.language === "english" ? ['Biography', myStory, 'read more...', '...read less'] : ['Биография', myStoryRU, 'больше...', '...меньше']
 
   return (
     <div id="bio">
 
         <div className="paragraph-title">
           <h3>{"<<<  "}</h3> 
-          <h1>Biography</h1>
+          <h1>{bioInfo[0]}</h1>
           <h3>{"  >>>"}</h3>
         </div>
        
@@ -39,8 +39,8 @@ const Bio = () => {
           <RotateImages images={images} imgInfo={imgInfo} anime={'fade'}/>
 
           <div id="my-story">
-            {readBttn ? myStory[0]: myStory.map( el=> el)}
-            <button type='button' onClick={()=> toggleBttn()}>{readBttn ? (  'read more...' ): '...read less'}</button>
+            {readBttn ? bioInfo[1][0]: bioInfo[1].map( el=> el)}
+            <button type='button' onClick={()=> toggleBttn()}>{readBttn ?  bioInfo[2] : bioInfo[3]}</button>
           </div>    
 
         </div> 
