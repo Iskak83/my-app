bd33c1ce7bb01b365f089df78fe251c73b3e046b
Iskak Mantyubetov <ayzik55@gmail.com>
2021-02-07 18:03:27 -0500

    added dafualt image for the site

--- src/components/hobbies.js
+++ src/components/hobbies.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from 'react'
 import scrollTo from 'gatsby-plugin-smoothscroll';
 import RotateImages from './RotateImages'
 import {hobbies} from './data.js'
@@ -20,23 +20,23 @@ const Hobby = () => {
   const images = [`gym.jpg`, `centre-court.jpg`,
   `nitto-final.jpg`, `cooking.jpg`]
 
-  const imgInfo = state.language === "english" ? ['LA Fitness, Baltimore', 'Centre Court in Wimbledon, London', 'ATP Nitto-Final in O2, London', "Cooking shepherd's pie home"] : ['LA Fitness, Балтимор', 'Центральный корт в Уимблдоне, Лондон', 'ATP Nitto-Final в O2, Лондон', 'Пастуший пирог']
+  const imgInfo = state.language === 'english' ? ['LA Fitness, Baltimore', 'Centre Court in Wimbledon, London', 'ATP Nitto-Final in O2, London', `Cooking shepherd's pie home`] : ['LA Fitness, Балтимор', 'Центральный корт в Уимблдоне, Лондон', 'ATP Nitto-Final в O2, Лондон', 'Пастуший пирог']
   
 
-  const hobbyInfo = state.language === "english" ? ['Hobbies', hobbies, 'read more...', '...read less'] : ['Xобби', hobbiesRU, 'больше...', '...меньше']
+  const hobbyInfo = state.language === 'english' ? ['Hobbies', hobbies, 'read more...', '...read less'] : ['Xобби', hobbiesRU, 'больше...', '...меньше']
   
     return (
-      <div id="hobby">
-          <div className="paragraph-title">
-            <h3>{"<<<  "}</h3> 
+      <div id='hobby'>
+          <div className='paragraph-title'>
+            <h3>{'<<<  '}</h3> 
             <h1>  {hobbyInfo[0]}  </h1>
-            <h3>{"  >>>"}</h3>
+            <h3>{'  >>>'}</h3>
           </div>
-          <div id="hobby-container">
+          <div id='hobby-container'>
 
             <RotateImages images={images} imgInfo={imgInfo} anime={'fade'} />
 
-            <div id="hobby-story">
+            <div id='hobby-story'>
             {readBttn ? (hobbyInfo[1][0]): hobbyInfo[1].map( el=> el)}
             
               <button type='button' onClick={()=> toggleBttn()}>{readBttn ? hobbyInfo[2] : hobbyInfo[3]}</button>
