8d5a67e2a039150b4b15761f4302c3e359a6a11f
Iskak Mantyubetov <ayzik55@gmail.com>
2021-02-08 14:32:00 -0500

    added dafualt image for the site

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,24 +7,24 @@ import './footer.scss';
 const Footer = () => {
 
 	return (
-		<footer className="footer-links" id="footer">
+		<footer className='footer-links' id='footer'>
 
-				<a href="mailto: ayzik55@gmail.com">
+				<a href='mailto: ayzik55@gmail.com'>
 					<FontAwesomeIcon icon={faEnvelope} />
 				</a>
-				<a href="https://www.linkedin.com/in/mantyubetov/" target="_blank" rel="noreferrer">
+				<a href='https://www.linkedin.com/in/mantyubetov/' target='_blank' rel='noreferrer'>
 					<FontAwesomeIcon icon={faLinkedin} />
 				</a>
-				<a href="https://github.com/Iskak83" target="_blank" rel="noreferrer">
+				<a href='https://github.com/Iskak83' target='_blank' rel='noreferrer'>
 					<FontAwesomeIcon icon={faGithub} />
 				</a>
-				<a href="https://twitter.com/iskak_man" target="_blank" rel="noreferrer">
+				<a href='https://twitter.com/iskak_man' target='_blank' rel='noreferrer'>
 					<FontAwesomeIcon icon={faTwitter} />
 				</a>
-				<a href="https://www.instagram.com/iskakmu/" target="_blank" rel="noreferrer">
+				<a href='https://www.instagram.com/iskakmu/' target='_blank' rel='noreferrer'>
 					<FontAwesomeIcon icon={faInstagram} />
 				</a>
-				<a href="https://www.facebook.com/iskak.mantyubetov" target="_blank" rel="noreferrer">
+				<a href='https://www.facebook.com/iskak.mantyubetov' target='_blank' rel='noreferrer'>
 					<FontAwesomeIcon icon={faFacebook} />
 				</a>
 			
