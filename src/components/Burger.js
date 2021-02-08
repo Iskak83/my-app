572c00868817eeab827b672f582e125981c0d38b
Iskak Mantyubetov <ayzik55@gmail.com>
2021-02-08 14:34:15 -0500

    added dafualt image for the site

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Burger = ({setBurger, burger}) => (
-<div id="burger-container">
-    <button onClick={() => setBurger(!burger)} id={burger ? "burger" : "x"}>
-        <div id="lineOne"></div>
-        <div id="lineTwo"></div>
-        <div id="lineThree"></div>
+<div id='burger-container'>
+    <button onClick={() => setBurger(!burger)} id={burger ? 'burger' : 'x'}>
+        <div id='lineOne'></div>
+        <div id='lineTwo'></div>
+        <div id='lineThree'></div>
     </button>
 </div>)
 
