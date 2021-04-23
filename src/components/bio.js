b3f0de526bdf8f335720f8ba8dea85d61b144eb8
Iskak Mantyubetov <ayzik55@gmail.com>
2021-04-23 17:00:30 -0400

    fixed toTop buttons centering

--- src/components/bio.js
+++ src/components/bio.js
@@ -9,7 +9,6 @@ import './bio.scss'
 const Bio = () => {
 
   const {state} = useContext(LanguageContext)
-
   const [readBttn, setReadBttn] = useState(true);
   
   function toggleBttn(){
@@ -18,7 +17,7 @@ const Bio = () => {
   }
 
   const images = [`virtual-grad.png`, `hyde-park.jpg`, `ala-archa.png`, `parade.png`, `a-default-site-image.jpg`]
-  
+
   const imgInfo =  state.language === 'english' ? [`Fullstack virtual graduation, South Orange`, `Hyde Park, London`, 'Ala-Archa Mountain, Kyrgyzstan', `Parade 2019, NYC`, 'Red Hook, NY'] : [`Виртуальный выпускной из Fullstack`,
   `Гайд-Парк, Лондон`, 'Горы Ала-Арча, Кыргызстан', `Парад 2019, Нью-Йорк`, 'Ред-Хук, Нью-Йорк']
 
@@ -32,15 +31,12 @@ const Bio = () => {
           <h1>{bioInfo[0]}</h1>
           <h3>{'  >>>'}</h3>
         </div>
-       
         <div id='bio-container'>
           <RotateImages images={images} imgInfo={imgInfo} anime={'fade'}/>
-
           <div id='my-story'>
             {readBttn ? bioInfo[1][0]: bioInfo[1].map( el=> el)}
             <button type='button' onClick={()=> toggleBttn()}>{readBttn ?  bioInfo[2] : bioInfo[3]}</button>
           </div>    
-
         </div> 
     </div>
   )
